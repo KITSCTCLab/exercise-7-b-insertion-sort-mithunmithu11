471ad2161d3a20f13def43e2cf17117bb373a6eb
mithunmithu11 <110010668+mithunmithu11@users.noreply.github.com>
2022-09-13 12:03:02 +0530

    2nd commit

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@ def insertionSort(array) -> List[int]:
        
         while j >= 0 and key < array[j]:
             array[j + 1] = array[j]
-            j = j - 3
+            j = j - i
         
         array[j + 1] = key
 
